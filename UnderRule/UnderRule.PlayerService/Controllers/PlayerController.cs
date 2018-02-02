db1c1de50857e747a8c318f1ca20538e78b1902f
Ryan Coates <Ryan.Coates@tombola.emea>
2018-02-02 10:14:12 +0000

    added basic persistance implementation

---
+++ UnderRule/UnderRule.PlayerService/Controllers/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnderRule.PlayerService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Player")]
+    public class PlayerController : Controller
+    {
+        PlayerService service = new PlayerService();
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return service.GetPlayer(id);
+        }
+    }
+}
---
+++ UnderRule/UnderRule.PlayerService/PlayerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnderRule.PlayerService
+{
+    public class PlayerService
+    {
+        internal string GetPlayer(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
