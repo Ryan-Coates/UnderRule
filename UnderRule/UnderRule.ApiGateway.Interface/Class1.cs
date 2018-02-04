b16a99867ec070faf824c3c8fe0541c0e4efd6d2
ryan-coates <ryan.coates@tombola.com>
2018-02-04 19:40:14 +0000

    added httprequester

--- UnderRule/UnderRule.ApiGateway.Interface/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace UnderRule.ApiGateway.Interface
-{
-    public class Class1
-    {
-    }
-}
---
+++ UnderRule/UnderRule.ApiGateway.Interface/HTTPRequester.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderRule.ApiGateway.Interface
+{
+    public class HTTPRequester
+    {
+        private readonly HttpClient client;
+
+        public HTTPRequester(string gatewayUrl)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(gatewayUrl);
+        }
+
+        internal async Task PostAsync(string path, HttpContent data)
+        {
+
+            var response = await client.PostAsync(path, data);
+
+            //todo: validate response, retry
+        }
+
+        internal async Task<List<string>> Get(string path, string authToken)
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
+
+            var response = await client.GetAsync(path);
+
+            //todo: validate response, retry
+
+            var responseString = response.Content.ReadAsStringAsync().Result;
+
+            List<string> values = JsonConvert.DeserializeObject<List<string>>(responseString);
+            return values;
+            //return responseString;
+        }
+    }
+}
