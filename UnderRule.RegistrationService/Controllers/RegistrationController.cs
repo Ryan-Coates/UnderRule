2b9d811cc4ea7a5d3aaa4ef5e9d18410c4f6f0ac
Shaneo234 <36048324+Shaneo234@users.noreply.github.com>
2018-02-08 20:47:55 +0000

    created registration service, wired up a controller and registraionModel, ur move

---
+++ UnderRule.RegistrationService/Controllers/RegistrationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UnderRule.RegistrationService.Models;
+
+namespace UnderRule.RegistrationService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Registration")]
+    public class RegistrationController : Controller
+    {
+        // POST api/values
+        [HttpPost]
+        public bool Post(RegistrationModel model) 
+        {
+            return true;
+
+        }
+    }
+
+
+}
---
+++ UnderRule.RegistrationService/Models/RegistrationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnderRule.RegistrationService.Models
+{
+    public class RegistrationModel
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+                
+    }
+}
