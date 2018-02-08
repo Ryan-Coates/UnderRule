207996591c13b9742ef50db2d3e0f90fa294cc1a
ryan-coates <ryan.coates@tombola.com>
2018-02-08 22:09:49 +0000

    fixed build error

--- UnderRule.RegistrationService/Controllers/RegistrationController.cs
+++ UnderRule.RegistrationService/Controllers/RegistrationController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using CommonObjects;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using UnderRule.RegistrationService.Models;
 using MySql.Data.MySqlClient;
 
 namespace UnderRule.RegistrationService.Controllers
@@ -29,7 +28,7 @@ namespace UnderRule.RegistrationService.Controllers
             //This is my connection string i have assigned the database file address path  
             string connectionString = "datasource=sarls.duckdns.org;port=3306;username=root;password=root_password";
             //This is my insert query in which i am taking input from the user through windows forms  
-            string Query = "insert into UnderRule.Users (username,password) values ('" + model.UserName + "','" + model.PassWord + "');";
+            string Query = "insert into UnderRule.Users (username,password) values ('" + model.UserName + "','" + model.Password + "');";
             //This is  MySqlConnection here i have created the object and pass my connection string.  
             MySqlConnection connection = new MySqlConnection(connectionString);
             //This is command class which will handle the query and connection object.  
