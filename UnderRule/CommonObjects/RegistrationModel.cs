c62d9e5f495e9a628a6ec1dff50b0e45b170fa2b
ryan-coates <ryan.coates@tombola.com>
2018-02-08 22:06:37 +0000

    added registration to api flow

---
+++ UnderRule/CommonObjects/RegistrationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonObjects
+{
+    public class RegistrationModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
